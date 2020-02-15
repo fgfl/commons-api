6c376e3bc3cb1b6353b88431c1013987df0013e8
Pascal van Leeuwen <commoddity@users.noreply.github.com>
2020-02-14 17:07:48 -0800

    Added first test file for User model and got it to passing.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    subject {
+      User.new( email: "gerald@dogs.com", 
+                name: "Gerald", 
+                password: "dogsrule", 
+                password_confirmation: "dogsrule",
+              )
+    }
+
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+  end
 end
