506fe6403148f7dc6143994d8c69202190fc3b33
fgfl <frederick.gfl@gmail.com>
2020-02-14 23:24:27 -0800

    Get legislative summary bills

--- app/uclassify/convert_xml_to_json.rb
+++ app/uclassify/convert_xml_to_json.rb
@@ -3,4 +3,10 @@ include XmlToJsonHelper
 
 file_data = File.read(__dir__ + "/training_data/publications_with_category.xml")
 
-File.write(__dir__ + "test.json", xml_to_json(file_data))
+# File.write(__dir__ + "/test.json", (xml_to_json(file_data)))
+items = JSON.parse(xml_to_json(file_data))["rss"]["channel"]["item"]
+bill_items = items.select do |item|
+  item["title"].include? "Legislative Summary"
+end
+
+pp bill_items[0..4]
