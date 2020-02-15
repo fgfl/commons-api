fdbb4b396064e8c494239c9e7be984a778ab7319
Pascal van Leeuwen <commoddity@users.noreply.github.com>
2020-02-14 16:36:15 -0800

    Added validations to db models.

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,3 +1,11 @@
 class Bill < ApplicationRecord
   belongs_to :session
+
+  validates :code, :presence => true
+  validates :title, :presence => true
+  validates :description, :presence => true
+  validates :tabled_date, :presence => true
+  validates :page_url, :presence => true
+  validates :full_text_url, :presence => true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+
   has_secure_password
+
+  validates :email, :presence => true, :uniqueness => true
+  validates_uniqueness_of :email, :case_sensitive => false
+  validates :name, :presence => true
+  validates :password, :confirmation => true, length: { minimum: 5 }
+  validates :password_confirmation, :presence => true
 
 end
