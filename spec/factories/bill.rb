d9e38fb1ca7ccc732c3ee51570e8a98c9149a548
Pascal van Leeuwen <commoddity@users.noreply.github.com>
2020-02-14 23:28:58 -0800

    Added facories for join tables and test frameworks for them.

--- spec/factories/bill.rb
+++ spec/factories/bill.rb
@@ -1,10 +1,9 @@
 FactoryBot.define do
-
   factory :bill do
     association :session
     code { "C-204" }
     title { "Canadian Bum Toucher Act" }
-    description { "An Act to touch bums."}
+    description { "An Act to touch bums." }
     tabled_date { Faker::Date.backward(days: 365) }
     page_url { "https://www.parl.ca/LegisInfo/BillDetails.aspx?Language=E&billId=10626760" }
     full_text_url { "https://parl.ca/DocumentViewer/en/10633126" }
@@ -14,10 +13,9 @@ FactoryBot.define do
     association :session
     code { "C-#{Faker::Number.number(digits: 2)}" }
     title { "#{Faker::Games::ElderScrolls.region} #{Faker::Relationship.parent} #{Faker::Verb.base} #{Faker::Games::Witcher.quote} Act" }
-    description { Faker::Quotes::Shakespeare.king_richard_iii_quote}
+    description { Faker::Quotes::Shakespeare.king_richard_iii_quote }
     tabled_date { Faker::Date.backward(days: 365) }
     page_url { "https://www.parl.ca/LegisInfo/BillDetails.aspx?Language=E&billId=10626760" }
     full_text_url { "https://parl.ca/DocumentViewer/en/10633126" }
   end
-  
 end
--- spec/models/user_bill_spec.rb
+++ spec/models/user_bill_spec.rb
@@ -1,5 +1,16 @@
-# require 'rails_helper'
+require "rails_helper"
 
-# RSpec.describe UserBill, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe UserBill, type: :model do
+  describe "Validations" do
+    let(:user_bill) { build(:user_bill) }
+
+    it "is valid with valid attributes" do
+      pp user_bill
+      expect(user_bill).to be_valid
+    end
+
+    # it "should save successfully" do
+    #   expect(user_bill.save).to eq(true)
+    # end
+  end
+end
