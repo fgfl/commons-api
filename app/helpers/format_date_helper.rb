fca06fe134f213a7df8ad6e3b2218236c8551133
Pascal van Leeuwen <commoddity@users.noreply.github.com>
2020-02-16 10:45:41 -0800

    dded last changes

--- app/helpers/format_date_helper.rb
+++ app/helpers/format_date_helper.rb
@@ -4,7 +4,7 @@ require 'date'
 
 # this method simply removes the time string from the pubDate field as it is redundant ('00:00:00 EST' for every item)
 
-# I was having issues storing the date as a date object, so I feel it's better to store as a string
+# [Pascal] I was having issues storing the date as a date object, so I feel it's better to store as a string
 # then convert the date to an actual Date object upon saving to the database using the following lines:
 # date_parsed = Date.parse(item['pubDate'])
 # item['pubDate'] = date_parsed
