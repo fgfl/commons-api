a0522791b44b928d966fc2f7a7ceb61616adeb1d
Pascal van Leeuwen <commoddity@users.noreply.github.com>
2020-02-17 00:15:16 -0800

    Added comments to fetch_helper file.

--- app/helpers/fetch_helper.rb
+++ app/helpers/fetch_helper.rb
@@ -5,14 +5,7 @@ require 'nokogiri'
 require 'open-uri'
 
 module FetchHelper
-  # def get_page_urls(array)
-  #   page_urls = []
-  #   array.each do |bill|
-  #     page_urls.push(bill['page_url'])
-  #   end
-  #   page_urls
-  # end
-
+  # this helper fetches the url for the full text of a given bill based on the page_url
   def get_full_text_url(bill)
     page_url = bill['page_url']
     page = Nokogiri::HTML(URI.open(page_url))
@@ -24,6 +17,7 @@ module FetchHelper
     full_text_url
   end
 
+  # this helper fetches the date that the bill was introduced at first reading from the bill summary page
   def get_introduced_date(page_url)
     page = Nokogiri::HTML(URI.open(page_url))
     date = page.text_equals('Introduction and First Reading').first.parent.parent.find('span').child.text
@@ -31,6 +25,7 @@ module FetchHelper
     introduced_date
   end
 
+  # this helper fetches the summary of the bill from the full text of the bill
   def get_description(full_text_url)
     full_text = Nokogiri::HTML(URI.open(full_text_url))
     summary_div = full_text.find(:xpath, ".//*[contains(text(), 'This enactment')]").text
--- spec/helpers/fetch_helper_spec.rb
+++ spec/helpers/fetch_helper_spec.rb
@@ -15,14 +15,6 @@ require 'rails_helper'
 RSpec.describe FetchHelper, type: :helper do
   test_data = [{ 'code' => 'C-204', 'title' => 'An Act to amend the Canadian Environmental Protection Act, 1999 (final disposal of plastic waste)', 'page_url' => 'https://www.parl.ca/LegisInfo/BillDetails.aspx?Language=E&billId=10626760' }, { 'code' => 'C-3', 'title' => 'An Act to amend the Royal Canadian Mounted Police Act and the Canada Border Services Agency Act and to make consequential amendments to other Acts', 'page_url' => 'https://www.parl.ca/LegisInfo/BillDetails.aspx?Language=E&billId=10613928' }, { 'code' => 'C-4', 'title' => 'An Act to implement the Agreement between Canada, the United States of America and the United Mexican States', 'page_url' => 'https://www.parl.ca/LegisInfo/BillDetails.aspx?Language=E&billId=10615191' }]
 
-  # describe 'GetPageUrls' do
-  #   it 'returns the page_url values of the bills array' do
-  #     test_page_urls = ['https://www.parl.ca/LegisInfo/BillDetails.aspx?Language=E&billId=10626760', 'https://www.parl.ca/LegisInfo/BillDetails.aspx?Language=E&billId=10613928', 'https://www.parl.ca/LegisInfo/BillDetails.aspx?Language=E&billId=10615191']
-
-  #     expect(helper.get_page_urls(test_data)).to eq(test_page_urls)
-  #   end
-  # end
-
   describe 'GetFullTextUrls' do
     it 'fetches the full_text_url for a bill' do
       test_bill = { 'code' => 'C-204', 'title' => 'An Act to amend the Canadian Environmental Protection Act, 1999 (final disposal of plastic waste)', 'page_url' => 'https://www.parl.ca/LegisInfo/BillDetails.aspx?Language=E&billId=10626760' }
