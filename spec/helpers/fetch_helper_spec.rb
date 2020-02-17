1b964f3babc8e00dbf0eb8e902f851722b944719
Pascal van Leeuwen <commoddity@users.noreply.github.com>
2020-02-17 00:17:31 -0800

    Added comments to fetch_helper file.

--- spec/helpers/fetch_helper_spec.rb
+++ spec/helpers/fetch_helper_spec.rb
@@ -15,7 +15,7 @@ require 'rails_helper'
 RSpec.describe FetchHelper, type: :helper do
   test_data = [{ 'code' => 'C-204', 'title' => 'An Act to amend the Canadian Environmental Protection Act, 1999 (final disposal of plastic waste)', 'page_url' => 'https://www.parl.ca/LegisInfo/BillDetails.aspx?Language=E&billId=10626760' }, { 'code' => 'C-3', 'title' => 'An Act to amend the Royal Canadian Mounted Police Act and the Canada Border Services Agency Act and to make consequential amendments to other Acts', 'page_url' => 'https://www.parl.ca/LegisInfo/BillDetails.aspx?Language=E&billId=10613928' }, { 'code' => 'C-4', 'title' => 'An Act to implement the Agreement between Canada, the United States of America and the United Mexican States', 'page_url' => 'https://www.parl.ca/LegisInfo/BillDetails.aspx?Language=E&billId=10615191' }]
 
-  describe 'GetFullTextUrls' do
+  describe 'GetFullTextUrl' do
     it 'fetches the full_text_url for a bill' do
       test_bill = { 'code' => 'C-204', 'title' => 'An Act to amend the Canadian Environmental Protection Act, 1999 (final disposal of plastic waste)', 'page_url' => 'https://www.parl.ca/LegisInfo/BillDetails.aspx?Language=E&billId=10626760' }
       test_full_text_url = 'https://parl.ca/DocumentViewer/en/10633126'
