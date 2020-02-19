aa8fbd49020f16f28127bba930a5faeb2e6ae308
Mitch Lum <mitchlum@Mitchs-MacBook-Pro.local>
2020-02-18 16:58:31 -0800

    changed password error messaging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
     else
       render json: { 
         status: 401,
-        errors: ['no such user', 'verify credentials and try again or signup']
+        errors: ['Invalid username or password.']
       }
     end
   end
