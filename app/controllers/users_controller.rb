f76881ce133c6f08648f76878a728a253aec97c7
fgfl <frederick.gfl@gmail.com>
2020-02-20 17:33:10 -0800

    Split the validation for password, so we can update without entering password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,15 +48,21 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find_by_id(params[:id])
-    puts "user:"
-    pp @user
-    puts "current_user"
-    pp current_user
-    puts "session:"
-    pp session[:user_id]
-    pp params.inspect
+
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+
+    params[:user].delete(:postal_code) if params[:user][:postal_code].blank?
+    params[:user].delete(:sms_notification) if params[:user][:sms_notification].blank?
+    params[:user].delete(:email_notification) if params[:user][:email_notification].blank?
+    params[:user].delete(:phone_number) if params[:user][:phone_number].blank?
+
     if @user == current_user
-      @user.update(user_params)
+      pp @user
+      pp user_params
+      @user.update!(user_params)
       render json: {
         status: 200,
         user: @user,
@@ -72,6 +78,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :sms_notification, :email_notification, :phone_number, :categories => [])
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :postal_code, :sms_notification, :email_notification, :phone_number, :categories => [])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-
   has_secure_password
 
   has_many :bill_users
@@ -11,8 +10,10 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :username, uniqueness: true, length: { minimum: 4 }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
   validates :email, presence: true, uniqueness: true
-  validates :password, confirmation: true, length: { minimum: 5 }
-  validates :password_confirmation, presence: true
+  validates :password, confirmation: true, length: { minimum: 5 }, on: :create
+  validates :password_confirmation, presence: true, on: :create
+  validates :password, confirmation: true, length: { minimum: 5 }, :if => :password, on: :update
+  validates :password_confirmation, presence: true, if: :password, on: :update
 
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
