3193082b7b9521385f7b92d17dd03e8fa7fb5b2f
Pascal van Leeuwen <commoddity@users.noreply.github.com>
2020-02-21 18:14:43 -0800

    uClassify organizer chain updated to use each bill_code to asingcategories.

--- app/interactors/classify_and_save_categories.rb
+++ app/interactors/classify_and_save_categories.rb
@@ -1,5 +1,8 @@
 class ClassifyAndSaveCategories
   include Interactor::Organizer
+  # PART TWO OF SAVING CATEGORIES
+  # Called by each iteration of the loop in GetTextFromFromBill
+  # Gets uClassify data for each bill and saves it to the billcategories join table
 
   organize FetchUclassifyData, FormatUclassifyData, SaveBillCategories
 end
