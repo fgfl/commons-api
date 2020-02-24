dad2f2003bcc007bb147ed76cfbb784f672aefd4
Pascal van Leeuwen <commoddity@users.noreply.github.com>
2020-02-24 12:26:42 -0800

    Got cookie situation working.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_commons-api', domain: 'your-frontend-domain'
+if Rails.env === "production"
+  Rails.application.config.session_store :cookie_store, key: "_commons-api", domain: :all
 else
-  Rails.application.config.session_store :cookie_store, key: '_commons-api'
+  Rails.application.config.session_store :cookie_store, key: "_commons-api"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@
 Rails.application.routes.draw do
   # match "*path", via: [:options], to: lambda { |_| [204, { "Content-Type" => "text/plain" }] }
   # Login related routes
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  get '/logged_in', to: 'sessions#is_logged_in?'
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/logged_in", to: "sessions#is_logged_in?"
 
   # Bill related routes
   resources :bills, only: [:index]
@@ -14,11 +14,11 @@ Rails.application.routes.draw do
 
   # User related routes
   resources :users, only: %i[create show index update]
-  post '/username_exists', to: 'users#username_exists'
-  post '/email_exists', to: 'users#email_exists'
+  post "/username_exists", to: "users#username_exists"
+  post "/email_exists", to: "users#email_exists"
 
   # User Watchlists
-  resources :bill_users, only: %i[create update]
+  resources :bill_users, only: %i[create]
   # User Categories
   resources :category_users, only: %i[create update]
 end
