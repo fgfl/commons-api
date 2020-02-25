d0bead49678bbcb888da2f11b46adad568ef3888
Pascal van Leeuwen <commoddity@users.noreply.github.com>
2020-02-24 18:56:10 -0800

    Chaned session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_commons-api'
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store, key: '_commons-api', domain: '*.herokuapp.com'
 else
-  Rails.application.config.session_store :cookie_store, key: '_commons-api'
+  Rails.application.config.session_store :cookie_store, key: '_commons-api', domain: '*.herokuapp.com'
 end
