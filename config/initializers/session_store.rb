a6a0e96b79dde349960fa663f393d8745575c2e6
Pascal van Leeuwen <commoddity@users.noreply.github.com>
2020-02-24 19:33:06 -0800

    Heroku cookie test

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_commons-api', domain: :any
+  Rails.application.config.session_store :cookie_store, key: '_commons-api', expire_after: 24.hours
 else
   Rails.application.config.session_store :cookie_store, key: '_commons-api'
 end
