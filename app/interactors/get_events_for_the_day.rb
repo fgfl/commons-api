c434ff2de9daa592bb3a80262e2aeb5add861c9e
fgfl <frederick.gfl@gmail.com>
2020-02-25 16:23:40 -0800

    Interactor to get events for day

---
+++ app/interactors/get_events_for_the_day.rb
@@ -0,0 +1,9 @@
+class GetEventsForTheDay
+  include Interactor
+
+  def call
+    events_for_day = Event.all.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+
+    context.events = events_for_day
+  end
+end
